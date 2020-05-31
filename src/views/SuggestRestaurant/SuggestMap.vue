2004edebb6d2ed62a48d7057177f83a84a0fa134
Andrew Wijaya <jun.quan.mail@gmail.com>
2020-05-31 22:20:26 +0700

    Add suggest restaurant page and integration

---
+++ src/views/SuggestRestaurant/SuggestMap.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Map v-bind:isAddMarker="isAddMarker" @onAddMarker="onAddMarker" />
+  </div>
+</template>
+
+<script>
+import Map from "@/components/Map.vue";
+import router from "@/router.js";
+
+export default {
+  components: {
+    Map,
+  },
+  data() {
+    return {
+      isAddMarker: true,
+    };
+  },
+  methods: {
+    onAddMarker(e) {
+      router.push(`/suggest/form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>

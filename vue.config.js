1a99562ab72bbbf3403dd34b7d27a180981d0109
Andrew Wijaya <jun.quan.mail@gmail.com>
2020-08-10 20:06:47 +0700

    change dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.(js|css)$/,
+      }),
+    ],
+  },
   assetsDir: "static",
   devServer: {
     https: true,
